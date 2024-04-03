72fe207ddb3d718be0eaac6073bded436f771976
Seven <phoenixpyra7@gmail.com>
2024-04-03 14:47:26 -0500

    added plant model

---
+++ server/models/Plant.js
@@ -0,0 +1,36 @@
+const { Schema } = require('mongoose');
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+const bookSchema = new Schema({
+  commonName: { 
+   type: String, 
+   required: true,
+},
+ scientificName: { 
+   type: String, 
+},
+  sunlight: {
+    type: String,
+  },
+  directOrIndirect: {
+    type: Boolean,
+
+  },
+  water: {
+    type: String,
+  },
+  annualOrPerennial: {
+    type: Boolean,
+  },
+ blooms: {
+    type: string,
+  },
+flowers: {
+Type: Boolean,
+  },
+deciduous: {
+Type: Boolean,
+  },
+});
+
+module.exports = bookSchema;
