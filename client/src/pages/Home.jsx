0c3b3e9d6d500e2631049b51599c5ebd266457cd
Seven <phoenixpyra7@gmail.com>
2024-04-05 00:20:25 -0500

    was able to add home page photo. changed photo. transparency, need to stop size at a min ratio

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { useQuery } from "@apollo/client";
 import { QUERY_USERS } from "../utils/queries";
-
 import UserList from "../components/UserList";
 
 function Home() {
@@ -18,12 +17,10 @@ function Home() {
 
   return (
     <>
-      {/* <div>Home</div> */}
-
-      <h1 className="block font-sans text-6xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 mb-3 text-center mx-auto">
-        PlantPal
-      </h1>
-      <UserList users={users} />
+        <h1 className="block font-sans text-6xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 mb-3 text-center mx-auto">
+          PlantPal
+        </h1>
+        <img src="./src/images/background2.png" style={{ width: '100%', height: '100%', objectFit: 'contain', opacity: '0.7'}} alt="background" />
     </>
   );
 }
