70407b3980b3781f2d6bdfed94cd95b71a26fb29
Seven <phoenixpyra7@gmail.com>
2024-04-04 19:35:26 -0500

    created about page and edited card layout for mobile friendly.

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -20,7 +20,7 @@ export default function Error() {
               to="/"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Go back home
+              Click me to go back to the Home page
             </Link>
 
           </div>
