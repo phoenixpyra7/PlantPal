01735e216c882be3d2fbdfa9eae3036d362c7b80
Seven <phoenixpyra7@gmail.com>
2024-04-05 21:51:12 -0500

    fixed margin inbetween menu selections when the hamberger menu opens.

--- client/src/components/NavBerger.jsx
+++ client/src/components/NavBerger.jsx
@@ -39,7 +39,7 @@ function Navberger() {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+            <div className="relative flex h-16 items-center justify-around">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -47,7 +47,13 @@ function Navberger() {
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <img
+                        className="h-8 w-8 rounded-full"
+                        src="./src/images/leaf1.png"
+                        alt="two leafs touching, light green and dark green"
+                      />
+
+                    // <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
@@ -89,9 +95,9 @@ function Navberger() {
                 </button>
 
               </div>
-                {/* Profile dropdown with Leaf Icon */}
+                
           <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+            <div className="space-y-1 space-x-8 px-4 pb-3 pt-2">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
