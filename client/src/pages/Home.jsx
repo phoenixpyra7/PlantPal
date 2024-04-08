6a7d06871298cd91a736a77a54fba2963b38c226
Seven <phoenixpyra7@gmail.com>
2024-04-07 21:34:20 -0500

    refactored the Home page so the title could lay on top of the background image.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+
+
 import { useQuery } from "@apollo/client";
 import { QUERY_USERS } from "../utils/queries";
 import UserList from "../components/UserList";
@@ -14,14 +16,52 @@ function Home() {
   if (loading) {
     return <h2>Loading…</h2>;
   }
+  
+  // Moved the SavedCollection component definition inside the Home component
+  const SavedCollection = () => {
+    const containerStyle = {
+      position: 'relative',
+      backgroundImage: `url('./src/images/background1.png')`,
+      backgroundSize: "cover", // Size of the background image
+      width: '100%',
+      height: '100%',
+      objectFit: 'contain',
+      opacity: '0.7',
+    };
+  
+    const h1Style = {
+      position: 'absolute',
+      top: '20%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: '1', // To ensure the h1 appears above the image
+      color: '#0a2a49', // Color of preference for title
+      fontSize: '6rem', // Font size 
+      fontWeight: 'bold', // Font weight
+      fontFamily: 'sans-serif', // Font family
+      textAlign: 'center', // Center the text
+    };
 
-  return (
-    <>
-        <h1 className="block font-sans text-6xl antialiased font-semibold leading-snug tracking-normal text-emerald-800 mb-3 text-center mx-auto">
+    return (
+      <div style={containerStyle}>
+        <img
+          src="./src/images/background2.png"
+          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+          alt="background image of a light green succulent"
+        />
+        <h1 style={h1Style}>
           PlantPal
         </h1>
-        <img src="./src/images/background2.png" bg-scroll style={{ width: '100%', height: '100%', objectFit: 'contain', opacity: '0.7'}} alt="background image of a light green succulent" />
-    </>
+      </div>
+    );
+  };
+
+  // Render the SavedCollection component
+  return (
+    <div>
+      <SavedCollection />
+      {/* Render other components or content here */}
+    </div>
   );
 }
 
--- client/src/pages/SavedCollection.jsx
+++ client/src/pages/SavedCollection.jsx
@@ -1,46 +1,50 @@
-import { useState } from 'react'
+import { useState } from "react";
 import React from "react";
 
+// const SavedCollection = () => {
+//   const containerStyle = {
+//     // backgroundImage: `url('./src/images/background1.png')`,
+//     // backgroundSize: "cover", // Adjust the size of the background image
+//     // img src="./src/images/background2.png" bg-scroll style={{ width: '100%', height: '100%', objectFit: 'contain', opacity: '0.7'}} alt="background image of a light green succulent" 
+//   };
 
+//   return (
+//     <div style={containerStyle}>
+//       {/* Your component content */}
+//       <div>
+//         <h1>My Collection</h1>
+//         <div>
+//           {/* TODO: add component for list of cards */}
+//           {/* Maybe card with cards nested inside */}
+//         </div>
+//         <div>
+//           <p>This is where we will keep the saved plants.</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SavedCollection;
 const SavedCollection = () => {
   const containerStyle = {
     backgroundImage: `url('./src/images/background1.png')`,
-    backgroundSize: 'cover', // Adjust the size of the background image
-    // Add other background properties as needed
+    backgroundSize: "cover", // Adjust the size of the background image
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain',
+    opacity: '0.9',
   };
 
   return (
     <div style={containerStyle}>
-      {/* Your component content */}
+      <img
+        src="./src/images/background1.png"
+        style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+        alt="background image of a light green succulent"
+      />
     </div>
   );
 };
 
 export default SavedCollection;
-
-
-
-
-
-// const MyCollection = ({}) => {
-
-
- 
-
-//   return (
-//     <div>
-//       <h1>My Collection</h1>
-//       <div>
-//      {/* TODO: add component for list of cards */}
-//       {/* Maybe card with cards nested inside */}
-//       </div>
-//       <div>
-        
-//       <p>This is where we will keep the saved plants.</p>
-//       </div>
-     
-//     </div>
-//   );
-// };
-
-// export default MyCollection;
